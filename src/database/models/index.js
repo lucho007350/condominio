f395ccfb23eed05340bf892ebe062a71d7634690
lucho007350 <luisfelipegrisalesramirez@gmail.com>
2025-10-08 19:45:44 -0500

    diagrams

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -4,11 +4,11 @@ const { Post, PostSchema } = require("./post.js");
 function setUpModels(sequelize) {
   // Inicialización
   Post.init(PostSchema, Post.config(sequelize));
-  Comment.init(CommentSchema, Comment.config(sequelize));
+
 
   // Relaciones
   Post.associate(sequelize.models);
-  Comment.associate(sequelize.models);
+
 }
 
 module.exports = setUpModels;
